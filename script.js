450ac48fa575a20bb4ad0eb0c322cbcf064db7d1
LuisOtv <luispim2005@gmail.com>
2025-03-20 17:26:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tab");
+
+  const board = [
+    document.querySelector(".stack"),
+    document.querySelector(".projects"),
+    document.querySelector(".tecnologies"),
+    document.querySelector(".more"),
+  ];
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabs.forEach((t) => t.classList.remove("selected"));
+      board.forEach((b) => (b.style.display = "none"));
+
+      tab.classList.add("selected");
+      board[index].style.display = "flex";
+    });
+  });
+
+  tabs[0].classList.add("selected");
+  board.forEach((b, i) => (b.style.display = i === 0 ? "flex" : "none"));
+});
